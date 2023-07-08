750956e188d9dd93ea5393a098b75a5b5e285803
Noman <noman92786@gmail.com>
2023-07-08 17:47:46 +0400

    Initial setup

---
+++ src/test/java/com/example/AvanzaSolutionsBookStore/AvanzaSolutionsBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AvanzaSolutionsBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanzaSolutionsBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
